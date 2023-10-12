cb44f6af587bbeb498ef537aff8d5e8be431b09e
Naveed Shahzad <lhrciit@gmail.com>
2023-10-12 13:02:58 +0500

    logo and text changed

--- resources/js/components/FooterComponent.vue
+++ resources/js/components/FooterComponent.vue
@@ -1,34 +1,41 @@
 <template>
     <footer class="main-footer footer-overlay">
+        <div class="row ml-0 mr-0">
+            <div class="col-lg-12 col-md-12 col-sm-12 mt-2 footer text-center">
+                <router-link :to="{ name: 'Overview' }">Overview</router-link>
+                <a
+                    target="_blank"
+                    href="https://business.punjab.gov.pk/starting-business"
+                    >Starting a Business</a
+                >
+                <a target="_blank" href="https://eodbpunjab.com">EoDB Punjab</a>
+                <a target="_blank" href="https://business.gov.pk/">PRMI</a>
+                <a target="_blank" href="https://invest.gov.pk/eodb"
+                    >EoDB Pakistan</a
+                >
+            </div>
+        </div>
 
-                <div class="row ml-0 mr-0">
-                    <div class="col-lg-12 col-md-12 col-sm-12 mt-2 footer text-center">
-                        <router-link  :to="{ name: 'Overview' }">Overview</router-link>
-                        <a target="_blank" href="https://business.punjab.gov.pk/starting-business">Starting a Business</a>
-                        <a target="_blank" href="https://eodbpunjab.com">EoDB Punjab</a>
-                        <a target="_blank" href="https://business.gov.pk/">PRMI</a>
-                        <a target="_blank" href="https://invest.gov.pk/eodb">EoDB Pakistan</a>
-                    </div>
+        <div class="row mt-2 ml-0 mr-0">
+            <div class="col-lg-6 col-md-6 col-sm-12">
+                <span class="nowrap footer2"
+                    >&copy; Automation of Business Regulations, Government of
+                    the Punjab</span
+                >
+            </div>
+            <div class="col-lg-6 col-md-6 col-sm-12 text-right">
+                <div class="nowrap1 footer2">
+                    Powered By: Punjab Information Technology Board
                 </div>
-
-                    <div class="row mt-2 ml-0 mr-0">
-                        <div class="col-lg-6 col-md-6 col-sm-12">
-							<span class="nowrap footer2">&copy; Automation of Business Regulations, Government of
-								Punjab</span>
-                        </div>
-                        <div class="col-lg-6 col-md-6 col-sm-12 text-right">
-                            <div class="nowrap1 footer2 ">Powered By: Punjab Information Technology Board</div>
-                        </div>
-                    </div>
+            </div>
+        </div>
     </footer>
 </template>
 
 <script>
 export default {
-    name: "FooterComponent"
-}
+    name: "FooterComponent",
+};
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
