1170d106df6c656265fe54369caf11f1b81453e2
Naveed Shahzad <lhrciit@gmail.com>
2025-01-15 16:36:52 +0500

    new page for rlco

--- resources/js/components/HeaderComponent.vue
+++ resources/js/components/HeaderComponent.vue
@@ -1,67 +1,159 @@
 <template>
-    <div class="preloader flex-column justify-content-center align-items-center" :class="{'hidden':usePreLoaderStore().isShow}">
+    <div
+        class="preloader flex-column justify-content-center align-items-center"
+        :class="{ hidden: usePreLoaderStore().isShow }"
+    >
         <div class="logo-container">
-            <img class="logo-animation" :src="useAssets('assets/ebiz_login_logo.svg')" alt="EBiz Logo">
+            <img
+                class="logo-animation"
+                :src="useAssets('assets/ebiz_login_logo.svg')"
+                alt="EBiz Logo"
+            />
             <div class="loading-circle"></div>
         </div>
     </div>
     <div class="container-fluid">
         <div class="row">
             <div class="col-12 px-0">
-                <nav id="eBiz-landingNavbar" class="navbar navbar-expand-lg position-fixed px-lg-5 px-4 bg-white z-3">
-                    <router-link class="navbar-brand" :to="{ name: 'home', hash: '#homeStartPage' }" @click.native="scrollToHash('homeStartPage')">
-                        <img :src="useAssets('assets/dash-logo1.svg')" alt="eBiz Logo" class="img-fluid">
+                <nav
+                    id="eBiz-landingNavbar"
+                    class="navbar navbar-expand-lg position-fixed px-lg-5 px-4 bg-white z-3"
+                >
+                    <router-link
+                        class="navbar-brand"
+                        :to="{ name: 'home', hash: '#homeStartPage' }"
+                        @click.native="scrollToHash('homeStartPage')"
+                    >
+                        <img
+                            :src="useAssets('assets/dash-logo1.svg')"
+                            alt="eBiz Logo"
+                            class="img-fluid"
+                        />
                     </router-link>
-                    <button class="navbar-toggler border-0 custom-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+                    <button
+                        class="navbar-toggler border-0 custom-toggler"
+                        type="button"
+                        data-bs-toggle="collapse"
+                        data-bs-target="#navbarTogglerDemo02"
+                        aria-controls="navbarTogglerDemo02"
+                        aria-expanded="false"
+                        aria-label="Toggle navigation"
+                    >
                         <span class="bar"></span>
                         <span class="bar"></span>
                         <span class="bar"></span>
                     </button>
 
-                    <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
-                        <ul class="nav nav-pills ms-auto justify-content-lg-between justify-content-start my-lg-0 my-3 pe-xl-5">
-                        <li class="nav-item">
-                                <router-link class="nav-link pb-1 px-1 mx-2" :to="{ name: 'home', hash: '#scrollspyHeading' }" @click.native="scrollToHash('scrollspyHeading')">
+                    <div
+                        class="collapse navbar-collapse"
+                        id="navbarTogglerDemo02"
+                    >
+                        <ul
+                            class="nav nav-pills ms-auto justify-content-lg-between justify-content-start my-lg-0 my-3 pe-xl-5"
+                        >
+                            <li class="nav-item">
+                                <router-link
+                                    class="nav-link pb-1 px-1 mx-2"
+                                    :to="{
+                                        name: 'home',
+                                        hash: '#scrollspyHeading',
+                                    }"
+                                    @click.native="
+                                        scrollToHash('scrollspyHeading')
+                                    "
+                                >
                                     <span></span> Home
                                 </router-link>
                             </li>
                             <li class="nav-item">
-                                <router-link class="nav-link pb-1 px-1 mx-2" :to="{ name: 'home', hash: '#scrollspyHeading1' }" @click.native="scrollToHash('scrollspyHeading1')">
+                                <router-link
+                                    class="nav-link pb-1 px-1 mx-2"
+                                    :to="{
+                                        name: 'home',
+                                        hash: '#scrollspyHeading1',
+                                    }"
+                                    @click.native="
+                                        scrollToHash('scrollspyHeading1')
+                                    "
+                                >
                                     <span></span> Business Sector
                                 </router-link>
                             </li>
                             <li class="nav-item">
-                                <router-link class="nav-link pb-1 px-1 mx-2" :to="{ name: 'home', hash: '#scrollspyHeading2' }" @click.native="scrollToHash('scrollspyHeading2')">
+                                <router-link
+                                    class="nav-link pb-1 px-1 mx-2"
+                                    :to="{
+                                        name: 'home',
+                                        hash: '#scrollspyHeading2',
+                                    }"
+                                    @click.native="
+                                        scrollToHash('scrollspyHeading2')
+                                    "
+                                >
                                     <span></span> Business Essentials
                                 </router-link>
                             </li>
                             <li class="nav-item">
-                                <router-link class="nav-link pb-1 px-1 mx-2" :to="{ name: 'home' , hash: '#scrollspyHeading3' }" @click.native="scrollToHash('scrollspyHeading3')">
+                                <router-link
+                                    class="nav-link pb-1 px-1 mx-2"
+                                    :to="{
+                                        name: 'home',
+                                        hash: '#scrollspyHeading3',
+                                    }"
+                                    @click.native="
+                                        scrollToHash('scrollspyHeading3')
+                                    "
+                                >
                                     <span></span> Connectivity
                                 </router-link>
                             </li>
                             <li class="nav-item">
-                                <router-link class="nav-link pb-1 px-1 mx-2" :to="{ name: 'home', hash: '#scrollspyHeading4' }" @click.native="scrollToHash('scrollspyHeading4')">
+                                <router-link
+                                    class="nav-link pb-1 px-1 mx-2"
+                                    :to="{
+                                        name: 'home',
+                                        hash: '#scrollspyHeading4',
+                                    }"
+                                    @click.native="
+                                        scrollToHash('scrollspyHeading4')
+                                    "
+                                >
                                     <span></span> Investments
                                 </router-link>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link pb-1 px-1 mx-2" href="#scrollspyHeading6">
-                                    <span>
-                                    </span> News
+                                <a
+                                    class="nav-link pb-1 px-1 mx-2"
+                                    href="#scrollspyHeading6"
+                                >
+                                    <span> </span> News
                                 </a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link pb-1 px-1 mx-2" href="#scrollspyHeading7">
-                                    <span>
-                                    </span> FAQs
+                                <a
+                                    class="nav-link pb-1 px-1 mx-2"
+                                    href="#scrollspyHeading7"
+                                >
+                                    <span> </span> FAQs
                                 </a>
                             </li>
                         </ul>
-                        <a :href="apply_ebiz_url" class="text-decoration-none eBizPortalBtn d-inline-flex align-items-center justify-content-center ps-3 pe-2">
+                        <a
+                            :href="apply_ebiz_url"
+                            target="_black"
+                            class="text-decoration-none eBizPortalBtn d-inline-flex align-items-center justify-content-center ps-3 pe-2"
+                        >
                             <span>eBiz Portal</span>
                             <span>
-                                <img :src="useAssets('assets/navbar_arrow_icon.svg')" alt="Navbar Icons" class="img-fluid">
+                                <img
+                                    :src="
+                                        useAssets(
+                                            'assets/navbar_arrow_icon.svg'
+                                        )
+                                    "
+                                    alt="Navbar Icons"
+                                    class="img-fluid"
+                                />
                             </span>
                         </a>
                     </div>
@@ -72,8 +164,8 @@
 </template>
 
 <script>
-import {useAssets} from "../composable/use-assets";
-import {usePreLoaderStore} from "../store/preloader";
+import { useAssets } from "../composable/use-assets";
+import { usePreLoaderStore } from "../store/preloader";
 
 export default {
     name: "HeaderComponent",
@@ -83,19 +175,23 @@ export default {
         scrollToHash(id) {
             // Helper function to adjust scroll position to the top 5%
             const scrollToTop7Percent = (element) => {
-                const topOffset = element.getBoundingClientRect().top + window.scrollY; // Get element's position relative to the viewport
+                const topOffset =
+                    element.getBoundingClientRect().top + window.scrollY; // Get element's position relative to the viewport
                 const adjustment = window.innerHeight * 0.07; // Calculate 7% of the viewport height
-                window.scrollTo({ top: topOffset - adjustment, behavior: 'smooth' });
+                window.scrollTo({
+                    top: topOffset - adjustment,
+                    behavior: "smooth",
+                });
             };
 
-            if (this.$route.name === 'home') {
+            if (this.$route.name === "home") {
                 const element = document.getElementById(id);
                 if (element) {
                     scrollToTop7Percent(element);
                 }
             } else {
                 // Redirect to home and scroll after navigation
-                this.$router.push({ name: 'home', hash: `#${id}` }).then(() => {
+                this.$router.push({ name: "home", hash: `#${id}` }).then(() => {
                     this.$nextTick(() => {
                         const element = document.getElementById(id);
                         if (element) {
@@ -104,7 +200,7 @@ export default {
                     });
                 });
             }
-        }
+        },
     },
     props: {
         totalFavorite: 0,
--- resources/js/views/Detail.vue
+++ resources/js/views/Detail.vue
@@ -1,8 +1,22 @@
 <template>
-    <div class="wrapping-searching mb-6">
-        <div class="row mt-3">
-            <div class="col-lg-12 col-md-12 col-sm-12">
-                <RlcoDetailComponent :rlco_detail="getDetailRlco" :isOverFlow="true"/>
+    <div
+        class="card shadow-none border-none"
+        style="margin-top: 70px; border: none"
+    >
+        <div class="card-body">
+            <div class="tab-content" id="eBizServicesTab1Content">
+                <div
+                    class="tab-pane fade show active"
+                    id="eBizService1-tab-pane"
+                    role="tabpanel"
+                    aria-labelledby="eBizService1-tab"
+                    tabindex="0"
+                >
+                    <RlcoDetailComponent
+                        :rlco_detail="getDetailRlco"
+                        :isOverFlow="true"
+                    />
+                </div>
             </div>
         </div>
     </div>
@@ -10,40 +24,45 @@
 
 <script>
 import RlcoDetailComponent from "../components/RlcoDetailComponent";
+import { usePreLoaderStore } from "../store/preloader";
 export default {
     name: "Detail",
     data() {
         return {
             rlco_detail: {},
             loading: false,
-        }
+        };
     },
     components: {
-        RlcoDetailComponent
+        RlcoDetailComponent,
+    },
+    created() {
+        usePreLoaderStore().setIsShow(false);
+        window.setTimeout(() => {
+            usePreLoaderStore().setIsShow(true);
+        }, 1000);
     },
     computed: {
-      getDetailRlco: function (){
-          return this.rlco_detail
-      }
+        getDetailRlco: function () {
+            return this.rlco_detail;
+        },
     },
     mounted() {
         this.loadRlcoDetail();
     },
     methods: {
-        loadRlcoDetail: function(){
+        loadRlcoDetail: function () {
             this.loading = true;
-            axios.get(`rlco-detail/${this.$route.params.id}`).then(response => {
-                this.rlco_detail = response.data.rlco_detail;
-                this.loading = false;
-            }).catch(error => {
-                this.loading = false;
-            })
-        }
-    }
-
-}
+            axios
+                .get(`rlco-detail/${this.$route.params.id}`)
+                .then((response) => {
+                    this.rlco_detail = response.data.rlco_detail;
+                    this.loading = false;
+                })
+                .catch((error) => {
+                    this.loading = false;
+                });
+        },
+    },
+};
 </script>
-
-<style scoped>
-
-</style>
